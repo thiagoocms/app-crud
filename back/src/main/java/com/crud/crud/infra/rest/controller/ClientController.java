485900e36e15b5b822af694312a5c32c56385c2e
Thiago Cavalcante <thiagocms13@gmail.com>
2023-03-06 21:38:59 -0300

    adicionando axios

--- back/src/main/java/com/crud/crud/infra/rest/controller/ClientController.java
+++ back/src/main/java/com/crud/crud/infra/rest/controller/ClientController.java
@@ -4,13 +4,14 @@ import com.crud.crud.infra.rest.dto.ClientDTO;
 import com.crud.crud.infra.service.ClientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/clients")
+@CrossOrigin("*")
 public class ClientController {
 
     private final ClientService clientService;
--- front/Resource/js/script.js
+++ front/Resource/js/script.js
@@ -1,5 +1,38 @@
-const btn = document.getElementById("btn")
+const url = "http://localhost:5000/api/clients"
+const btnCriar = document.getElementById("btnCriar")
+const test = document.getElementById("test")
+const result = document.getElementById("result")
 
-btn.addEventListener("click",()=>{
-    alert(1)
-})
+
+
+
+ btnCriar.addEventListener("click",()=>{
+
+  
+  //criarrcd
+  axios.post(url,getDados())
+    .then(response => {
+      const data =  response.data
+    
+      test.textContent = JSON.stringify(data)
+      
+      console.log(data)
+      window.location.href = 'index.html'
+      
+
+    })
+    .catch(error => console.log(error))
+ })
+
+ const getDados = ()=>{
+  const client ={
+    name: document.querySelector("#name").value ,
+    fone: document.querySelector("#fone").value ,
+    email: document.querySelector("#email").value ,
+    job: document.querySelector("#job").value,
+    birth: document.querySelector("#birth").value
+  } 
+  return client
+ }
+
+
